323e1a4181f5b3160e4543800d3782d5c4017ac5
aleazar-axr <aleazarjohnvillanueva@gmail.com>
2026-07-30 00:20:18 +0800

    Add root route to server.js

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,10 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 const apiRoutes = require('./routes/apiRoutes');
 app.use('/api', apiRoutes);
 
+app.get('/', (req, res) => {
+    res.send('WMSU Portal API is running!');
+});
+
 // --- START SERVER ---
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
